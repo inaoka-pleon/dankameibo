e68cfeb5db82bd79215421300edb30adc5d42294
pleon-082 <inaoka@pleon-082>
2025-03-06 14:00:39 +0900

    同名戒名検索画面作成

---
+++ app/Http/Controllers/KaimyouCheckController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Danka;
+use App\Services\CommonUtility;
+use Illuminate\Http\Request;
+
+class KaimyouCheckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $put_flg = false;
+        if (strcmp($request->searchType, 'kaimyou_search') === 0) {
+            $put_flg = true;
+        }
+
+        //一覧検索パラメータ取得
+        $cond_kaimyoucheck = CommonUtility::GetQueryParameter($request, 'cond_kaimyoucheck', $put_flg);
+
+        $query = Danka::query()
+                    ->join('followers as chief', function($join) {
+                        $join->on('dankas.id', '=', 'chief.danka_id')
+                            ->where('chief.chiefmourner_flg', '=', 1);
+                    })
+                    ->leftJoin('followers as deceased', function($join) {
+                        $join->on('dankas.id', '=', 'deceased.danka_id')
+                            ->where('deceased.deceased_flg', '=', 1);
+                    })
+                    ->select('dankas.id',
+                            'dankas.area',
+                            'chief.name',
+                            'chief.namekana',
+                            'deceased.kaimyou',
+                            'deceased.zokumyou',
+                            'deceased.relationship')
+                    ->where('chief.deceased_flg', '=', 0)
+                    ->where('deceased.chiefmourner_flg', '=', 0)
+                    ->orderby('chief.namekana', 'asc');
+
+        if(!empty($cond_kaimyoucheck['kaimyou'])) {
+         $query->where('deceased.kaimyou', 'like', '%'.$cond_kaimyoucheck['kaimyou'].'%');
+        }
+        if(!empty($cond_kaimyoucheck['zokumyou'])) {
+            $query->where('deceased.zokumyou', 'like', '%'.$cond_kaimyoucheck['zokumyou'].'%');
+        }
+        if(!empty($cond_kaimyoucheck['relationship'])) {
+            $query->where('deceased.relationship', 'like', '%'.$cond_kaimyoucheck['relationship'].'%');
+        }
+
+        $kaimyouchecks = $query->paginate(10);
+
+        // 該当件数表示
+        $kaimyouCount = $kaimyouchecks->total();
+
+        return view('kaimyouchecks.index', compact('kaimyouchecks', 'kaimyouCount'))
+            ->with('cond_kaimyoucheck', $cond_kaimyoucheck);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/kaimyouchecks/index.blade.php
@@ -0,0 +1,113 @@
+<x-app-layout>
+    <link rel="stylesheet" href="/css/style.css" >
+    <link rel="stylesheet" href="/css/app.css" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- エラーの表示を追加 -->
+    @include('errors.form_errors')
+
+    <main class="mt-1 py-4 px-2 sm:px-4">
+        <div class="flex flex-col justify-center items-center z-10"></div>
+        <div class="mb-4 flex flex-col justify-center items-center">
+            <div class="flex justify-between bg-white border-l-8 border-b-2 border-slate-500 shadow-sm py-3 sm:py-4 px-4 sm:px-4 rounded-bl w-full max-w-7xl">
+                <div class="text-gray-800 text-xl font-semibold">同名戒名検索</div>
+            </div>
+        </div>
+        <div class="flex flex-col justify-center items-center mb-8">
+            <div class="mb-4 w-full max-w-7xl">
+                <div id="search_head" class="w-full px-4 md:px-6 py-2 text-left text-lg font-normal bg-custom-5 border border-b-2 border-gray-200 hover:underline hover:cursor-pointer active:underline rounded-t open">
+                    <svg class="w-6 h-6 -mt-1 inline-flex mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/200/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
+                    条件指定
+                </div>
+                <div id="search_body" class="px-6 sm:px-4 md:px-6 py-4 bg-white shadow-md border border-gray-200 rounded-b-xl" style="display: block;">
+                    <form aciton="{{ route('kaimyoucheck.index') }}" method="GET" >
+                        <div class="grid grid-cols-12">
+                            <label for="k_ksimyou" class="block col-span-12 sm:col-span-2 sm:py-4 pr-4 pl-2 sm:pl-0 font-semibold text-gray-700 sm:text-right text-sm sm:text-base">戒名</label>
+                            <div class="col-span-12 sm:col-span-4 py-1 sm:py-2 sm:pl-2">
+                                <input type="text" name="cond_kaimyoucheck[kaimyou]" value="{{ $cond_kaimyoucheck['kaimyou'] ?? null }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus-ring-opacity-50 w-full" id="k_kaimyou">
+                            </div>
+                            <label for="k_ksimyou" class="block col-span-12 sm:col-span-2 sm:py-4 pr-4 pl-2 sm:pl-0 font-semibold text-gray-700 sm:text-right text-sm sm:text-base">俗名</label>
+                            <div class="col-span-12 sm:col-span-4 py-1 sm:py-2 sm:pl-2">
+                                <input type="text" name="cond_kaimyoucheck[zokumyou]" value="{{ $cond_kaimyoucheck['zokumyou'] ?? null }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus-ring-opacity-50 w-full" id="k_zokumyou">
+                            </div>
+                            <label for="k_ksimyou" class="block col-span-12 sm:col-span-2 sm:py-4 pr-4 pl-2 sm:pl-0 font-semibold text-gray-700 sm:text-right text-sm sm:text-base">関係</label>
+                            <div class="col-span-12 sm:col-span-4 py-1 sm:py-2 sm:pl-2">
+                                <input type="text" name="cond_kaimyoucheck[relationship]" value="{{ $cond_kaimyoucheck['relationship'] ?? null }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus-ring-opacity-50 w-full" id="k_relationship">
+                            </div>
+                        </div>
+                        <div class="mt-3 flex items-center sm:justify-end">
+                            <button type="submit" class="btn-primary proc-btn text-xs shadow-sm py-3 px-4 mr-3" name="searchType" value="kaimyou_search">指定条件で検索する</button>
+                            <button type="button" class="btn-default text-xs reset shadow-sm py-3 px-4">指定条件をリセットする</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr class="w-full mb-4 max-w-7xl">
+            <div class="w-full md:mb-4 max-w-7xl text-sm sm:text-base">
+                <div class="">
+                    @if ($kaimyouchecks->isNotEmpty()) 
+                        <div class="mb-4">
+                            <p class="ml-2">該当：{{ $kaimyouCount }} 件</p>
+                        </div>
+                        <table class="table-danka radius-table shadow">
+                            <thead>
+                                <tr class>
+                                    <th class="kaimyou_name">氏名</th>
+                                    <th class="kaimyou_area">地区名</th>
+                                    <th class="kaimyou_kaimyou">戒名</th>
+                                    <th class="kaimyou_zokumyou">俗名</th>
+                                    <th class="kaimyou_relationship">関係</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($kaimyouchecks as $kaimyoucheck)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('danka.show', $kaimyoucheck->id )}}" class="blue_line">{{ $kaimyoucheck->name }}</a>
+                                        </td>
+                                        <td>{{ $kaimyoucheck->area }}</td>
+                                        <td>{{ $kaimyoucheck->kaimyou }}</td>
+                                        <td>{{ $kaimyoucheck->zokumyou }}</td>
+                                        <td>{{ $kaimyoucheck->relationship }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <br>
+                        <div class="pagination justify-content-center">
+                            {{ $kaimyouchecks->appends([
+                                'cond_kaimyoucheck' => @(Request::get('cond_danka')),
+                                ])->links() }}
+                        </div>
+                    @else
+                        <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+                            <p class="font-bold">戒名情報がありません</p>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const searchHead = document.getElementById('search_head');
+            const searchBody = document.getElementById('search_body');
+
+            searchHead.addEventListener('click', function() {
+                if (searchBody.style.display === 'none' || searchBody.style.display === '') {
+                    searchBody.style.display = 'block';
+                } else {
+                    searchBody.style.display = 'none';
+                }
+            });
+
+            document.querySelector('.reset').addEventListener('click', function() {
+                document.querySelectorAll('#search_body input[type="text"]').forEach(input => input.value = '');
+                document.querySelectorAll('#search_body select').forEach(select => select.selectedIndex = 0);
+            });
+        });
+    </script>
+</x-app-layout>
